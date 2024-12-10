d42ad4c8d19fe85b653881102703c4d7ab64b6b9
Jogász <benjamin.biro.bgy@gmail.com>
2024-12-10 12:42:06 +0100

    Two forms merged into one (MainForm) to stay in one window Welcoming screen done (background added) Screen to select number of rounds done Need to make questions, possible asnwers for them (one of them being correct), make them appear randomly and one question appear max once per game. Give the User 10s to answer, 3 mistakes and game is over.

--- quizGame/MainForm.Designer.cs
+++ quizGame/MainForm.Designer.cs
@@ -28,70 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
-            lblTitle = new Label();
-            lblCreator = new Label();
-            btnStart = new Button();
             SuspendLayout();
             // 
-            // lblTitle
-            // 
-            lblTitle.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            lblTitle.AutoSize = true;
-            lblTitle.BackColor = Color.Transparent;
-            lblTitle.Font = new Font("Showcard Gothic", 65F);
-            lblTitle.ForeColor = Color.White;
-            lblTitle.Location = new Point(42, 84);
-            lblTitle.Name = "lblTitle";
-            lblTitle.Size = new Size(507, 109);
-            lblTitle.TabIndex = 0;
-            lblTitle.Text = "Quiz game";
-            // 
-            // lblCreator
-            // 
-            lblCreator.AutoSize = true;
-            lblCreator.BackColor = Color.Transparent;
-            lblCreator.Font = new Font("Showcard Gothic", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblCreator.ForeColor = SystemColors.AppWorkspace;
-            lblCreator.Location = new Point(216, 193);
-            lblCreator.Name = "lblCreator";
-            lblCreator.Size = new Size(154, 33);
-            lblCreator.TabIndex = 1;
-            lblCreator.Text = "by Jogasz";
-            // 
-            // btnStart
-            // 
-            btnStart.BackColor = Color.BlueViolet;
-            btnStart.Cursor = Cursors.Hand;
-            btnStart.Font = new Font("Showcard Gothic", 60F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btnStart.ForeColor = Color.White;
-            btnStart.Location = new Point(63, 501);
-            btnStart.Name = "btnStart";
-            btnStart.Size = new Size(464, 154);
-            btnStart.TabIndex = 2;
-            btnStart.Text = "Start";
-            btnStart.UseVisualStyleBackColor = false;
-            // 
-            // Form1
+            // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.Teal;
-            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackColor = Color.White;
             ClientSize = new Size(584, 761);
-            Controls.Add(btnStart);
-            Controls.Add(lblCreator);
-            Controls.Add(lblTitle);
-            Name = "Form1";
+            Name = "MainForm";
             Text = "Form1";
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-
-        private Label lblTitle;
-        private Label lblCreator;
-        private Button btnStart;
     }
 }
--- quizGame/MainForm.cs
+++ quizGame/MainForm.cs
@@ -2,30 +2,143 @@ namespace quizGame
 {
     public partial class MainForm : Form
     {
+
         public MainForm()
         {
             InitializeComponent();
+            this.DoubleBuffered = true;
+            InitializeForm();
 
-            this.Size = new Size(600, 800);
+            //Form starter background
+            string imagePath = Path.Combine(Application.StartupPath, "pics", "startBackground.jpg");
+            this.BackgroundImage = Image.FromFile(imagePath);
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+
+            //Title
+            Label lblTitle = new Label
+            {
+                Text = "quizimodo",
+                TextAlign = ContentAlignment.MiddleCenter,
+                Font = new Font("Showcard Gothic", 60),
+                ForeColor = Color.White,
+                BackColor = Color.Transparent,
+                Size = new Size(500, 100),
+                Location = new Point(50, 100),
+            };
+
+            //Author
+            Label lblAuthor = new Label
+            {
+                Text = "by Jogasz",
+                TextAlign = ContentAlignment.MiddleCenter,
+                Font = new Font("Showcard Gothic", 20),
+                ForeColor = Color.Gray,
+                BackColor = Color.Transparent,
+                Size = new Size(200, 50),
+                Location = new Point(0, 750),
+            };
+
+            //Start button
+            Button btnStart = new Button
+            {
+                Text = "Start",
+                TextAlign = ContentAlignment.MiddleCenter,
+                Font = new Font("Showcard Gothic", 40),
+                BackColor = Color.Black,
+                ForeColor = Color.White,
+                Cursor = Cursors.Hand,
+                Size = new Size(400, 150),
+                Location = new Point(100, 500),
+            };
+
+            btnStart.FlatStyle = FlatStyle.Flat;
+            btnStart.FlatAppearance.BorderSize = 2;
+            btnStart.FlatAppearance.BorderColor = Color.White;
+            btnStart.FlatAppearance.MouseOverBackColor = Color.Gray;
+            btnStart.FlatAppearance.MouseDownBackColor = Color.DarkGray;
+
+            this.Controls.Add(lblTitle);
+            this.Controls.Add(lblAuthor);
+            this.Controls.Add(btnStart);
+
+            btnStart.Click += roundSelect;
+        }
+
+        //Form
+        private void InitializeForm()
+        {
+            this.ClientSize = new Size(600, 800);
             this.FormBorderStyle = FormBorderStyle.Fixed3D;
             this.StartPosition = FormStartPosition.CenterScreen;
             this.MaximizeBox = false;
             this.Text = "Quiz game";
-
-            btnStart.FlatStyle = FlatStyle.Flat;
-            btnStart.FlatAppearance.BorderSize = 0;
-            btnStart.FlatAppearance.MouseOverBackColor = Color.DarkSlateBlue;
-            btnStart.FlatAppearance.MouseDownBackColor = Color.Indigo;
-
-            btnStart.Click += BtnStart_Click;
         }
 
-        private void BtnStart_Click(object? sender, EventArgs e)
+        private async void roundSelect(object? sender, EventArgs e)
         {
-            this.Hide();
+            this.Controls.Clear();
 
-            GameForm gameForm = new GameForm();
-            gameForm.Show();
+            // Round number options
+            Button btnOption1 = new Button
+            {
+                Text = "5 rounds",
+                TextAlign = ContentAlignment.MiddleCenter,
+                Font = new Font("Showcard Gothic", 50),
+                BackColor = Color.Black,
+                ForeColor = Color.White,
+                Cursor = Cursors.Hand,
+                Size = new Size(400, 150),
+                Location = new Point(100, 100),
+            };
+
+            btnOption1.FlatStyle = FlatStyle.Flat;
+            btnOption1.FlatAppearance.BorderSize = 2;
+            btnOption1.FlatAppearance.BorderColor = Color.White;
+            btnOption1.FlatAppearance.MouseOverBackColor = Color.Gray;
+            btnOption1.FlatAppearance.MouseDownBackColor = Color.DarkGray;
+            btnOption1.Click += (s, e) => Rounds(5);
+
+            Button btnOption2 = new Button
+            {
+                Text = "7 rounds",
+                TextAlign = ContentAlignment.MiddleCenter,
+                Font = new Font("Showcard Gothic", 50),
+                BackColor = Color.Black,
+                ForeColor = Color.White,
+                Cursor = Cursors.Hand,
+                Size = new Size(400, 150),
+                Location = new Point(100, 300),
+            };
+
+            btnOption2.FlatStyle = FlatStyle.Flat;
+            btnOption2.FlatAppearance.BorderSize = 2;
+            btnOption2.FlatAppearance.BorderColor = Color.White;
+            btnOption2.FlatAppearance.MouseOverBackColor = Color.Gray;
+            btnOption2.FlatAppearance.MouseDownBackColor = Color.DarkGray;
+            btnOption2.Click += (s, e) => Rounds(7);
+
+            Button btnOption3 = new Button
+            {
+                Text = "12 rounds",
+                TextAlign = ContentAlignment.MiddleCenter,
+                Font = new Font("Showcard Gothic", 50),
+                BackColor = Color.Black,
+                ForeColor = Color.White,
+                Cursor = Cursors.Hand,
+                Size = new Size(400, 150),
+                Location = new Point(100, 500),
+            };
+
+            btnOption3.FlatStyle = FlatStyle.Flat;
+            btnOption3.FlatAppearance.BorderSize = 2;
+            btnOption3.FlatAppearance.BorderColor = Color.White;
+            btnOption3.FlatAppearance.MouseOverBackColor = Color.Gray;
+            btnOption3.FlatAppearance.MouseDownBackColor = Color.DarkGray;
+            btnOption3.Click += (s, e) => Rounds(12);
+
+            this.Controls.Add(btnOption1);
+            this.Controls.Add(btnOption2);
+            this.Controls.Add(btnOption3);
         }
     }
 }
